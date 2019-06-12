e7301a4b1e713a7b00f5a8565bbafff714b1e289
Jason Leach <jlspursfan@gmail.com>
2019-06-12 20:07:15 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,18 @@
-CREATE TABLE projects;
+CREATE TABLE projects (
+  title TEXT PRIMARY KEY,
+    category TEXT,
+    funding_goal INTEGER,
+    start_date TEXT,
+    end_date TEXT
+);
 
-CREATE TABLE users;
+CREATE TABLE users(
+  name TEXT PRIMARY KEY,
+    age INTEGER
+);
 
-CREATE TABLE pledges;
+CREATE TABLE pledges(
+  amount INTEGER PRIMARY KEY,
+  project_id INTEGER,
+  user_id INTEGER
+);
